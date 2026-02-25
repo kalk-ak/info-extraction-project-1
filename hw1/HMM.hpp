4e2d7bc125b95b3326bd8ee81b78d67a794de51d
Kaleb Aklilu <kalebaklilu3@gmail.com>
2026-02-24 23:37:54 -0500

    Added a matrix for the transition and emission probabilities

--- hw1/HMM.hpp
+++ hw1/HMM.hpp
@@ -8,7 +8,7 @@ class HMM
     //
     // Responsible for reading the dataset and storing it in memory and initilizing the parameters
     // randomly
-    HMM(std::filesystem::path training_dataset);
+    HMM(std::filesystem::path training_dataset, int len_data);
 
     // Destructor for the HMM. Responsible for closing the file if needed and releasing the
     // initilized data from memory
@@ -23,10 +23,20 @@ class HMM
     // Pointer to the dataset. Allocated in the constructor and deallocated in the destructor
     // Using the heap so that the code can be used for much bigger datasets than the memory can hold
     // at once. The data can be loaded in batches in the train function
-    std::vector<char> *data;
+    std::vector<char> data;
 
-    // All these probabilities are initilized when the Constructor is called and updated when the
-    // train function is called
+    // NOTE: n x n matrix where n is the number of states in the HMM
+    std::vector<std::vector<double>>
+        trainsition_probabilities; // Matrix representing the transition probabilities between
+                                   // states
+                                   // NOTE: has size num_states x num_observations
+
+    std::vector<std::vector<double>>
+        emission_probabilities; // Matrix representing the emission probabilities of the
+                                // observations given the states
+
+    // All these probabilities are initilized when the Constructor is called and updated when
+    // the train function is called
     std::vector<double> alpha; // cache for forward probabilities
     std::vector<double> beta;  // cache for backward probabilities
     std::vector<double> gamma; // cache for state probabilities
