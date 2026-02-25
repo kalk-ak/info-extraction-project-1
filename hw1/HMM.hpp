c6642a5d1ec7bc519308286e61078a69af0f35f2
Kaleb Aklilu <kalebaklilu3@gmail.com>
2026-02-24 23:21:38 -0500

    Added a vector to cache the alpha and the beta values

--- hw1/HMM.hpp
+++ hw1/HMM.hpp
@@ -1,4 +1,5 @@
 #include <filesystem>
+#include <vector>
 
 class HMM
 {
@@ -19,11 +20,18 @@ class HMM
   private:
     std::filesystem::path training_dataset;
 
+    // All these probabilities are initilized when the Constructor is called and updated when the
+    // train function is called
+    std::vector<double> alpha; // cache for forward probabilities
+    std::vector<double> beta;  // cache for backward probabilities
+    std::vector<double> gamma; // cache for state probabilities
+
     // TODO: Create a helper function to load the data in memory
     // or initilize a generator for it
 
     // called from train function to update the trainsitions
     void update_transition();
 
-    // called from the
+    // Called from the train function to update the emission probabilities
+    void update_emission();
 };
