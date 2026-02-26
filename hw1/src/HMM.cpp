8eb39e99bfd0d85ea4d8cb1d428020c87ec6ab24
Kaleb Aklilu <kalebaklilu3@gmail.com>
2026-02-25 22:37:51 -0500

    Created the update transmition and emission helper function

--- hw1/src/HMM.cpp
+++ hw1/src/HMM.cpp
@@ -188,7 +188,6 @@ void HMM<T>::initialize_trainsition_probabilities(int num_states, bool randomly,
             "Must provide weights for the transition probabilities or set randomly flag to true");
     }
 
-    // if
     if (not randomly)
     {
         this->trainsition_probabilities = weights;
@@ -216,7 +215,7 @@ void HMM<T>::initialize_trainsition_probabilities(int num_states, bool randomly,
             for (int j = 0; j < num_states; ++j)
             {
                 double probability = dis(gen);
-                distribution[i][i] = probability;
+                distribution[i][j] = probability;
                 normalization_sum += probability;
             }
 
@@ -271,8 +270,9 @@ void HMM<T>::initialize_initial_state_probabilities(bool randomly,
     }
     else
     {
-        int num_states = this->states.size(); // number of unique characters in the dataset, which
-                                              // is the number of states and observations in the HMM
+
+        int num_states =
+            this->trainsition_probabilities.size(); // Use the actual number of HMM states
 
         // variable to modify the original transition probabilities
         std::vector<double> &distribution = this->initial_state_probabilities;
@@ -321,18 +321,18 @@ template <typename T> void HMM<T>::train(int num_itterations)
     }
 
     // cahce the number of states and the number of steps in the dataset for easy access
-    const size_t numStates =
+    const size_t num_states =
         this->trainsition_probabilities.size(); // Use the actual number of HMM states
-    const size_t numSteps = this->data.size();
+    const size_t len_data = this->data.size();
 
     // initialize the alpha, beta, and gamma vectors
-    this->alpha.assign(numStates, std::vector<double>(numSteps, 0.0));
-    this->beta.assign(numStates, std::vector<double>(numSteps, 0.0));
-    this->gamma.assign(numStates, std::vector<double>(numSteps, 0.0));
+    this->alpha.assign(num_states, std::vector<double>(len_data, 0.0));
+    this->beta.assign(num_states, std::vector<double>(len_data, 0.0));
+    this->gamma.assign(num_states, std::vector<double>(len_data, 0.0));
 
     // Initialize alpha at t=0 in LOG SPACE
     int first_char_index = this->states_to_index[this->data[0]];
-    for (size_t state = 0; state < numStates; state++)
+    for (size_t state = 0; state < num_states; state++)
     {
         // log(initial_prob * emission_prob) -> log(initial_prob) + log(emission_prob)
         this->alpha[state][0] = std::log(this->initial_state_probabilities[state]) +
@@ -340,8 +340,8 @@ template <typename T> void HMM<T>::train(int num_itterations)
     }
 
     // Initialize beta at the final time step in LOG SPACE
-    for (size_t state = 0; state < numStates; state++)
-        this->beta[state][numSteps - 1] = 0.0; // log(1.0) = 0.0
+    for (size_t state = 0; state < num_states; state++)
+        this->beta[state][len_data - 1] = 0.0; // log(1.0) = 0.0
 
     // Run Baum-Welch
     for (int iter = 0; iter < num_itterations; iter++)
@@ -400,28 +400,25 @@ template <typename T> void HMM<T>::calculate_alpha_beta_gamma()
     }
 
     // Calculate the backward probabilities (beta) next
-    for (int i = length_of_data - 1; i >= 0; i++)
+    for (int t = length_of_data - 2; t >= 0; t--)
     {
-        // Cache the current data and its index
-        T current_observation = this->data[i];
-        int current_observation_index = this->states_to_index[current_observation];
+        T next_observation = this->data[t + 1];
+        int next_obs_index = this->states_to_index[next_observation];
 
-        for (int j = 0; j < length_of_states; j++)
+        for (int i = 0; i < length_of_states; i++) // length_of_states is your hidden states
         {
-            double log_sum = -std::numeric_limits<double>::infinity(); // Represents log(0)
+            double log_sum = -std::numeric_limits<double>::infinity();
 
-            for (int k = 0; k < length_of_states; k++)
+            for (int j = 0; j < length_of_states; j++)
             {
-                // beta[k][i+1] * transition[j][k] * emission[k][current_observation_index]
-                // := log_beta + log_transition + log_emission
-                double current_val =
-                    beta[k][i + 1] + std::log(this->trainsition_probabilities[j][k]) +
-                    std::log(this->emission_probabilities[k][current_observation_index]);
+                // log(transition[i][j]) + log(emission[j][next_obs]) + beta[j][t+1]
+                double current_val = std::log(this->trainsition_probabilities[i][j]) +
+                                     std::log(this->emission_probabilities[j][next_obs_index]) +
+                                     beta[j][t + 1];
 
                 log_sum = log_sum_exp(log_sum, current_val);
             }
-
-            beta[j][i] = log_sum;
+            beta[i][t] = log_sum;
         }
     }
 
@@ -438,8 +435,79 @@ template <typename T> void HMM<T>::calculate_alpha_beta_gamma()
 
 template <typename T> void HMM<T>::update_transition()
 {
+    int numSteps = this->data.size();
+    int numStates = this->trainsition_probabilities.size();
+
+    for (int i = 0; i < numStates; i++)
+    {
+        double log_denominator = -std::numeric_limits<double>::infinity();
+        std::vector<double> log_numerators(numStates, -std::numeric_limits<double>::infinity());
+
+        for (int j = 0; j < numStates; j++)
+        {
+            double log_num_ij = -std::numeric_limits<double>::infinity();
+
+            // Loop through time steps 1 to T-1
+            for (int t = 1; t < numSteps; t++)
+            {
+                int obs_index = this->states_to_index[this->data[t]];
+
+                // xi_t(i,j) = alpha[i][t-1] * trans[i][j] * emit[j][O_t] * beta[j][t]
+                double current_xi =
+                    this->alpha[i][t - 1] + std::log(this->trainsition_probabilities[i][j]) +
+                    std::log(this->emission_probabilities[j][obs_index]) + this->beta[j][t];
+
+                log_num_ij = log_sum_exp(log_num_ij, current_xi);
+            }
+            log_numerators[j] = log_num_ij;
+            log_denominator = log_sum_exp(log_denominator, log_num_ij);
+        }
+
+        // Update the actual transition matrix
+        for (int j = 0; j < numStates; j++)
+        {
+            // Division in log space is subtraction
+            double log_prob = log_numerators[j] - log_denominator;
+
+            // Convert back to normal space
+            this->trainsition_probabilities[i][j] = std::exp(log_prob);
+        }
+    }
 }
 
 template <typename T> void HMM<T>::update_emission()
 {
+    int numSteps = this->data.size();
+    int numStates = this->trainsition_probabilities.size();
+    int numSymbols = this->sorted_states.size();
+
+    for (int j = 0; j < numStates; j++)
+    {
+        double log_denominator = -std::numeric_limits<double>::infinity();
+        std::vector<double> log_numerators(numSymbols, -std::numeric_limits<double>::infinity());
+
+        for (int t = 0; t < numSteps; t++)
+        {
+            // gamma_t(j) = alpha_t(j) * beta_t(j)
+            // Note: We can just use the gamma matrix you already calculated!
+            double current_gamma = this->gamma[j][t];
+
+            // Add to the total expected time spent in state j (Denominator)
+            log_denominator = log_sum_exp(log_denominator, current_gamma);
+
+            // Add to the expected time spent in state j emitting specific character (Numerator)
+            int obs_index = this->states_to_index[this->data[t]];
+            log_numerators[obs_index] = log_sum_exp(log_numerators[obs_index], current_gamma);
+        }
+
+        // Update the actual emission matrix
+        for (int k = 0; k < numSymbols; k++)
+        {
+            // Division in log space is subtraction
+            double log_prob = log_numerators[k] - log_denominator;
+
+            // Convert back to normal space
+            this->emission_probabilities[j][k] = std::exp(log_prob);
+        }
+    }
 }
